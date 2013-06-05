a94b6a28097cfdce0bb368bc9711523736d7342d
Nathan Krishnan <nathan@new-host.home>
2013-06-04 23:59:57 -0400

    Updated framework

--- framework/application/config/config.php
+++ framework/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/helloworld/framework/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
