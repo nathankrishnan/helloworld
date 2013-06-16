575e59ad897acca8930b4ca9e4be23997126aa26
unknown <yijiefeng@gmail.com>
2013-06-15 21:31:47 -0400

    adsf

--- ci/application/views/results.php
+++ ci/application/views/results.php
@@ -1,9 +1,6 @@
 
 <?php
 
-	//echo "<h1>Route From ".$from." To ".$to.": </h1>";
-	//echo "<hr/>";
-
 	echo $from."<br/>";
 	echo $to."<br/>";
 
@@ -21,12 +18,51 @@
 	$to_lines = explode("|", $to);
 
 
-/*
-	echo $to_station."<br/>";
-	foreach ($to_lines as $line) {
-		echo $line."<br/>";
+	echo "<h1>Route From ".$from_station." To ".$to_station.": </h1>";
+	echo "<hr/>";
+
+	$jsonStations = Metro::getJson("http://api.wmata.com/Rail.svc/json/JStations?api_key=", "2d3sva9w5mmhrc9wdaq3m2z3");
+
+
+	foreach($from_lines as $from_line) {
+		//if the two stations are on the same line
+		if($from_line === $to_lines[0] || $from_line ===$to_lines[1]){
+			echo "take the ".$from_line." line from ".$from_station." to ".$to_station;
+			break;
+		}
+		//if the two stations aren't on the same line
+		else{
+			foreach($jsonStations->Stations as $station){
+				if($from_line === $station->LineCode1){
+					if($station->LineCode2 === $to_lines[0]){
+						echo "take the ".$from_line." line from ".$from_station." to ".$station->Name."<br/><br/>";
+						echo "transfer to the ".$to_lines[0]." line and take it down to ".$to_station;
+						break;
+					}
+					elseif($station->LineCode2 === $to_lines[1]){
+						echo "take the ".$from_line." line from ".$from_station." to ".$station->Name."<br/><br/>";
+						echo "transfer to the ".$to_lines[1]." line and take it down to ".$to_station;
+						break;
+					}
+				}
+				elseif($from_line === $station->LineCode2){
+					if($station->LineCode1 === $to_lines[0]){
+						echo "take the ".$from_line." line from ".$from_station." to ".$station->Name."<br/><br/>";
+						echo "transfer to the ".$to_lines[0]." line and take it down to ".$to_station;
+						break;
+					}
+					elseif($station->LineCode1 === $to_lines[1]){
+						echo "take the ".$from_line." line from ".$from_station." to ".$station->Name."<br/><br/>";
+						echo "transfer to the ".$to_lines[1]." line and take it down to ".$to_station;
+						break;
+					}
+				
+				}
+			}
+		}
 	}
-*/
+
+
 
 
 ?>
